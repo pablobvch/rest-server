16c844cf0a87408499c510bb367a5b05e1e0c0d7
Pablo Alaniz <pablobvch@gmail.com>
2022-04-07 17:58:18 -0300

    First commit

---
+++ models/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usuariosPath = "/api/usuarios";
+    //Middlewares
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    //directorio public
+    this.app.use(express.static("public"));
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use(this.usuariosPath, require("../routes/usuarios"));
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Rest server corriendo en puerto", this.port);
+    });
+  }
+}
+
+module.exports = Server;
